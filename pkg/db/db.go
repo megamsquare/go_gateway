a60a3711e8acf7e1a4f4a55255d1d3bcb744e74a
Michael <msquaremega@gmail.com>
2023-04-26 16:14:08 +0100

    add database configuration

---
+++ pkg/db/db.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	config "github.com/megamsquare/go_gateway/pkg/env_config"
+)
+
+type DBConfig struct {
+	Username string `config:"DB_USERNAME" default:"gateway_postgres"`
+	Password string `config:"DB_PASSWORD" default:"gateway_postgres"`
+	Database string `config:"DB_NAME" default:"gateway_postgres"`
+	Address  string `config:"DB_ADDRESS" default:"localhost"`
+	Port     int    `config:"DB_PORT" default:"5432"`
+}
+
+func Load_config() *DBConfig {
+	var conf DBConfig
+	config.Load(&conf)
+	return &conf
+}
+
+
+func Connect_db(conf *DBConfig) (*sql.DB, error) {
+	conn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", conf.Address, conf.Port, conf.Username, conf.Password, conf.Database)
+
+	db, err := sql.Open("postgres", conn)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
