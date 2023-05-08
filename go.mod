73f00489c53de42a922a7b11e3cc4b818412423e
Michael <msquaremega@gmail.com>
2023-05-08 18:21:23 +0100

    add queue logic to the app

--- go.mod
+++ go.mod
@@ -5,3 +5,5 @@ go 1.19
 require github.com/kelseyhightower/envconfig v1.4.0
 
 require github.com/gorilla/mux v1.8.0
+
+require github.com/streadway/amqp v1.0.0 // indirect
---
+++ pkg/queue/queue.go
@@ -0,0 +1,64 @@
+package queue
+
+import (
+	"fmt"
+	"log"
+
+	config "github.com/megamsquare/go_gateway/pkg/env_config"
+	"github.com/streadway/amqp"
+)
+
+type RabbitMQ struct {
+	conn *amqp.Connection
+	channel *amqp.Channel
+}
+
+type RabbitMQConfig struct {
+	Username string `config:"RABBITMQ_USER" default:"rabbitmq"`
+	Password string `config:"RABBITMQ_PASSWORD" default:"rabbitmq"`
+	Host     string `config:"RABBITMQ_HOST" default:"rabbitmq"`
+	Port     int    `config:"RABBITMQ_PORT" default:"5672"`
+}
+
+func Load_config() *RabbitMQConfig {
+	var conf RabbitMQConfig
+	config.Load(&conf)
+	return &conf
+}
+
+func Connect_rabbitMQ(conf *RabbitMQConfig) (*RabbitMQ, error) {
+	rabbitMQURL := fmt.Sprintf("amqp://%s:%s@%s:%d", conf.Username, conf.Password, conf.Host, conf.Port)
+
+	conn, err := amqp.Dial(rabbitMQURL)
+	if err != nil {
+		return nil, err
+	}
+
+	channel, err := conn.Channel()
+	if err != nil {
+		return nil, err
+	}
+
+	rabbitMQ := &RabbitMQ{
+		conn:    conn,
+		channel: channel,
+	}
+
+	return rabbitMQ, nil
+}
+
+func (r *RabbitMQ) Close() {
+	if r.channel != nil {
+		err := r.channel.Close()
+		if err != nil {
+			log.Printf("Error closing RabbitMQ channel: %v", err)
+		}
+	}
+
+	if r.conn != nil {
+		err := r.conn.Close()
+		if err != nil {
+			log.Printf("Error closing RabbitMQ connectio: %v", err)
+		}
+	}
+}
